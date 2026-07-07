dfba427118c4825424224a483530d2eef3641ccd
kevinraipane <raipanekn@gmail.com>
2026-07-07 11:29:46 -0300

    Initial commit

---
+++ src/test/java/com/example/GestorStock/GestorStockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.GestorStock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestorStockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
