22a228d3bf84d3f6a92629ec7ca28bf931c409a5
camiarjona <camarjona01@gmail.com>
2026-07-07 12:30:36 -0300

    feature - creación de enums

---
+++ src/main/java/com/example/GestorStock/purchase_order/entity/enums/PurchaseOrderStatus.java
@@ -0,0 +1,7 @@
+package com.example.GestorStock.purchase_order.entity.enums;
+
+public enum PurchaseOrderStatus {
+    DRAFT,
+    RECEIVED,
+    CANCELLED;
+}
---
+++ src/main/java/com/example/GestorStock/stock_movement/entity/enums/MovementType.java
@@ -0,0 +1,8 @@
+package com.example.GestorStock.stock_movement.entity.enums;
+
+public enum MovementType {
+    INBOUND,
+    OUTBOUND,
+    TRANSFER,
+    ADJUSTMENT;
+}
