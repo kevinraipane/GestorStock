04b24432d0132fdc05c0d6e1068563f98c60f0cc
camiarjona <camarjona01@gmail.com>
2026-07-13 11:10:46 -0300

    feature - creación entidad PurchaseOrder

---
+++ src/main/java/com/example/GestorStock/purchase_order/entity/PurchaseOrder.java
@@ -0,0 +1,48 @@
+package com.example.GestorStock.purchase_order.entity;
+
+import com.example.GestorStock.purchase_order.entity.enums.PurchaseOrderStatus;
+import com.example.GestorStock.purchase_order_item.entity.PurchaseOrderItem;
+import com.example.GestorStock.supplier.entity.Supplier;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "purchase_orders")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Builder
+public class PurchaseOrder {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idPurchaseOrder;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private PurchaseOrderStatus status;
+
+    @Column(name = "ordered_at", nullable = false, updatable = false)
+    private LocalDateTime orderedAt;
+
+    @Column(name = "received_at")
+    private LocalDateTime receivedAt;
+
+    @Column(columnDefinition = "TEXT")
+    private String notes;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "supplier_id", nullable = false)
+    private Supplier supplier;
+
+    @OneToMany(mappedBy = "purchaseOrder", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<PurchaseOrderItem> purchaseOrderItems = new ArrayList<>();
+
+    @OneToMany (mappedBy = "purchaseOrder", cascade = CascadeType.ALL)
+    private List<StockMovement> stockMovements = new ArrayList<>();
+}
