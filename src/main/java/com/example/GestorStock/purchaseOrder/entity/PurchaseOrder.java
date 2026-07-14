3a376777f9996bb8a941d88f29e38f1e6402b15b
kevinraipane <raipanekn@gmail.com>
2026-07-14 19:09:39 -0300

    update - PurcharseOrder
    Se agrega el metodo PrePersist

--- src/main/java/com/example/GestorStock/purchaseOrder/entity/PurchaseOrder.java
+++ src/main/java/com/example/GestorStock/purchaseOrder/entity/PurchaseOrder.java
@@ -1,7 +1,9 @@
-package com.example.GestorStock.purchase_order.entity;
+package com.example.GestorStock.purchaseOrder.entity;
 
-import com.example.GestorStock.purchase_order.entity.enums.PurchaseOrderStatus;
-import com.example.GestorStock.purchase_order_item.entity.PurchaseOrderItem;
+
+import com.example.GestorStock.purchaseOrder.entity.enums.PurchaseOrderStatus;
+import com.example.GestorStock.purchaseOrderItem.entity.PurchaseOrderItem;
+import com.example.GestorStock.stockMovement.entity.StockMovement;
 import com.example.GestorStock.supplier.entity.Supplier;
 import jakarta.persistence.*;
 import lombok.*;
@@ -40,9 +42,22 @@ public class PurchaseOrder {
     @JoinColumn(name = "supplier_id", nullable = false)
     private Supplier supplier;
 
+    @Builder.Default
     @OneToMany(mappedBy = "purchaseOrder", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<PurchaseOrderItem> purchaseOrderItems = new ArrayList<>();
 
-    @OneToMany (mappedBy = "purchaseOrder", cascade = CascadeType.ALL)
+    @Builder.Default
+    @OneToMany (mappedBy = "purchaseOrder")
     private List<StockMovement> stockMovements = new ArrayList<>();
+
+    @PrePersist
+    protected void onCreate() {
+        if (this.orderedAt == null) {
+            this.orderedAt = LocalDateTime.now();
+        }
+
+        if (this.status == null) {
+            this.status = PurchaseOrderStatus.DRAFT;
+        }
+    }
 }
