43e7c0a546fe9f9648e56f2d7dc0ef567fedc2c3
kevinraipane <raipanekn@gmail.com>
2026-07-14 19:06:46 -0300

    fix - correcion nombre clase

--- src/main/java/com/example/GestorStock/salesOrderItem/entity/SalesOrderItem.java
+++ src/main/java/com/example/GestorStock/salesOrderItem/entity/SalesOrderItem.java
@@ -15,7 +15,7 @@ import java.math.BigDecimal;
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
-public class SaleOrderItem {
+public class SalesOrderItem {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
